d0cf5ab6a7d67256cf6f314e2daf18014263cf34
NicholasSlade <60986212+NicholasSlade@users.noreply.github.com>
2021-12-12 21:21:06 -0800

    -added bootstrap -setup login and register pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Navbar from './components/Navbar/Navbar';
-import Login from './pages/Login/Login'
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
 
-// Component
 function App() {
 
   // State / Hooks
@@ -21,8 +22,7 @@ function App() {
       <Router>
         <Routes>
           <Route path='/' element={<Login />} />
-          <Route />
-          <Route />
+          <Route path='/register' element={<Register />} />
         </Routes>
       </Router>
     </div>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,31 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import { Navbar, Container } from 'react-bootstrap';
+import './Navbar.css';
 
-const navBar = typeof token === Object ? <div>Token is a string</div> : <div>Token is not a string</div>
+const navBar = (
+    <Navbar className="navbar" variant="dark">
+      <Container>
+        <Navbar.Brand href="/">Mibrary</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>
+            Login
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+)
 
-function Navbar () {
+function NavBar () {
 
   let [token, setToken] = useState({ token: 12345});
-  useEffect(() => { console.log(typeof token)});
+
+  const location = useLocation();
+
+
 
   return ( navBar )
 }
 
-export default Navbar;
+export default NavBar;
