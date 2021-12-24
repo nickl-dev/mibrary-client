c7d33b5a2c604fd63c9759e2c4a79b161becf76d
NicholasSlade <60986212+NicholasSlade@users.noreply.github.com>
2021-12-23 22:32:24 -0800

    setting state for inputs -created dummy submit function

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,20 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom'
 import logo from '../../logo.svg';
+import axios from 'axios'
 import './Register.css'
 
 function Register () {
+
+  const [userInputs, setUserInputs] = useState({
+    username: '',
+    email: '',
+    password: ''
+  })
+
+  function handleChange (event) {
+    const { name, value } = event.target
+    setUserInputs({ ...userInputs, [name]: value })
+    console.log(name, value)
+  }
+
+  async function handleSubmit (event) {
+    event.preventDefault()
+    if (userInputs) {
+      try {
+        await axios.post(`${process.env.REACT_APP_API_BASE_URL}register`, userInputs)
+        .then(res => console.log(res))
+        .catch(err => console.error(err))
+      } catch (err) {
+        console.error(err)
+      }
+    } else return
+  }
+
   return (
     <div className="register d-flex flex-column align-items-center justify-content-center">
       <h1 className="register__heading">Register</h1>
 
     <img src={logo} className="logo my-5" alt="Spinning book" />
 
-      <Form>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Username</Form.Label>
+          <Form.Control type="text" placeholder="Username" name="username" onChange={handleChange} />
+        </Form.Group>
+
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email Address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control type="email" placeholder="Enter email" name="email" onChange={handleChange} />
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
           </Form.Text>
@@ -22,7 +54,7 @@ function Register () {
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control type="password" placeholder="Password" name="password" onChange={handleChange} />
         </Form.Group>
 
         <Button variant="primary" type="submit">
