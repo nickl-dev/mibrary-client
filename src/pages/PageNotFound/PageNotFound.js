1434f4d8cdd90f9fc66bc2f040b4be626079b4ce
Nicholas Lal <nlalkta@gmail.com>
2022-09-10 15:39:57 -0600

    -styled 404 page

--- src/pages/PageNotFound/PageNotFound.js
+++ src/pages/PageNotFound/PageNotFound.js
@@ -4,10 +4,30 @@ function PageNotFound() {
   const isLoggedIn = false;
 
   return (
-    <div>
-      <h1>404</h1>
-      <p>The page you're looking for can't be found.</p>
-      <Link to={isLoggedIn ? '/my-reading-list' : '/'}>Take me back</Link>
+    <div
+      className='d-flex flex-column justify-content-center align-items-center px-3'
+      style={{ height: 'calc(100vh - 56px)' }}
+    >
+      <h1
+        className='fw-bold'
+        style={{ fontSize: '60px' }}
+      >
+        404
+      </h1>
+
+      <p
+        className='text-center m-0'
+        style={{ fontSize: '18px' }}
+      >
+        The page you are looking for cannot be found.
+      </p>
+
+      <Link
+        className='mt-3'
+        to={isLoggedIn ? '/my-reading-list' : '/'}
+      >
+        Take me back
+      </Link>
     </div>
   )
 
